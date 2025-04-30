63fca7fc51c44b31efb4a9ea95422ab059122108
phamvanduy1008 <phamvanduy.dev@gmail.com>
2025-04-30 10:42:48 +0700

    C010: Shop page handel add to cart

---
+++ app/page/account/acc/favourite.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const favourite = () => {
+  return (
+    <View><Text>favourite</Text></View>
+  )
+}
+
+export default favourite
